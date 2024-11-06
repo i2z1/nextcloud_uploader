10f1c54f635121bf8cd1515b07df0ea71df931b4
i2z1 <i2z1@yandex.ru>
2024-11-06 13:42:12 +0300

    add code styling

--- nextcloud_upload.R
+++ nextcloud_upload.R
@@ -9,8 +9,7 @@ nc_uri <- "https://example.com"
 auth_str <- "<nc_login>:<nc_password>"
 src_fld <- src_path
 
-nc_make_folder <- function(fld_path, nc_path, nc_uri = nc_uri, auth_str = auth_str){
-
+nc_make_folder <- function(fld_path, nc_path, nc_uri = nc_uri, auth_str = auth_str) {
   fld_path <- gsub(" ", "\ ", fld_path)
 
   nc_user <- strsplit(auth_str, ":")[[1]] %>% .[1]
@@ -21,18 +20,16 @@ nc_make_folder <- function(fld_path, nc_path, nc_uri = nc_uri, auth_str = auth_s
   cli::cli_alert_info("Creating folder {full_nc_uri}")
 
   res_cmd <- system2("curl", args = c("-X", "MKCOL", "-u", auth_str, full_nc_uri))
-  if(res_cmd==0){
+  if (res_cmd == 0) {
     cli::cli_alert_success("Created folder {fld_path} at")
     cli::cli_alert_info("{full_nc_uri}")
     return(TRUE)
   } else {
     return(FALSE)
   }
-
 }
 
-nc_upl_file <- function(src_path, nc_path, nc_uri = nc_uri, auth_str = auth_str){
-
+nc_upl_file <- function(src_path, nc_path, nc_uri = nc_uri, auth_str = auth_str) {
   src_path <- gsub(" ", "\ ", src_path)
 
   file_name <- basename(src_path)
@@ -49,7 +46,7 @@ nc_upl_file <- function(src_path, nc_path, nc_uri = nc_uri, auth_str = auth_str)
 
   res_cmd <- system2("curl", args = c("-X", "PUT", "-u", auth_str, full_nc_uri, "-T", src_path))
 
-  if(res_cmd==0){
+  if (res_cmd == 0) {
     cli::cli_alert_info("Uploaded file {src_path} to {nc_path}")
     return(TRUE)
   } else {
@@ -63,10 +60,12 @@ nc_upl_file <- function(src_path, nc_path, nc_uri = nc_uri, auth_str = auth_str)
 #' @param nc_path path to NC
 #' @param nc_uri NC URL with https://
 #' @param auth_str auth string i.e. "login:password" string
-nc_upl_folder <- function(src_fld, nc_path, nc_uri = nc_uri, auth_str = auth_str){
+nc_upl_folder <- function(src_fld, nc_path, nc_uri = nc_uri, auth_str = auth_str) {
   ndirs <- list.dirs(src_fld, recursive = F, full.names = T)
-  upl_files <- list.files(src_fld, all.files = T,
-                          full.names = T)
+  upl_files <- list.files(src_fld,
+    all.files = T,
+    full.names = T
+  )
   nfiles <- setdiff(upl_files, ndirs) %>%
     setdiff(., paste0(src_fld, "/", c(".", "..")))
 
@@ -76,7 +75,6 @@ nc_upl_folder <- function(src_fld, nc_path, nc_uri = nc_uri, auth_str = auth_str
 
   purrr::walk(nfiles, nc_upl_file, new_nc_path, nc_uri, auth_str)
   purrr::walk(ndirs, nc_upl_folder, new_nc_path, nc_uri, auth_str)
-
 }
 
 nc_upl_folder(src_path, nc_path, nc_uri, auth_str)
